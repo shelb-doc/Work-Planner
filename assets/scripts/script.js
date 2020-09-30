7a3f02e01a63717d57eb10b578b0e913c08cc4bc
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-09-29 23:08:03 -0400

    preformed a function test and linked pages

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    console.log("This is loading!");
+    // DOM VARIABLES
+    // JS VARIABLES
+    // FUNCTION DEFINITIONS
+    // FUNCTION CALLS
+    // EVENT LISTENERS
+});
