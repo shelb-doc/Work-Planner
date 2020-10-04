1e7268518ded7b6d81232f27bafad4ec1f73b81f
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-04 18:31:58 -0400

    added save function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
-    console.log("This is loading!");
-    // console.log(moment().format('H'));
     // DOM VARIABLES
     // JS VARIABLES
+    myStorage = window.localStorage;
     // FUNCTION DEFINITIONS
-
     function twelveHr(hour){
         if (hour > 12){
             return ((hour - 12) + "PM")
@@ -16,22 +14,24 @@ $(document).ready(function () {
     function renderDate(){
         $("#currentDay").text (moment().format('MMMM Do YYYY, h:mm:ss a'));
     }
-
     function renderPlanner(){
         var currentTime = moment().format('H');
         var currentClass = "";
         for(i=0; i<9; i++){
             var time = twelveHr(9 + i)
-            if (currentTime == (9 + i)){
-                currentClass = "present";
-            } else if (currentTime > (9 + i)) {
+            if (currentTime > (9 + i)){
                 currentClass = "past";
+            } else if (currentTime == (9 + i)) {
+                currentClass = "present";
             } else {
                 currentClass = "future";
             }
+            if(myStorage.getItem(time) == null){
+                myStorage.setItem(time, " ");
+            }
             var content = '<form class="row">'
             content += '<div class="col-md-1 hour">' + (time) + '</div>';
-            content += '<div class="col-md-10 description p-0"><textarea class="'+ currentClass +'" id="'+ (time) +'"></textarea>' + '</div>';
+            content += '<div class="col-md-10 description p-0"><textarea class="'+ currentClass +'" id="'+ (time) +'">'+ myStorage.getItem(time) +'</textarea></div>';
             content += '<button class="col-md-1 saveBtn"><i class="far fa-save fa-lg"></i></button>';
             content += "</form>"
             $('.container').append(content);
@@ -42,9 +42,11 @@ $(document).ready(function () {
     setInterval(renderDate, 1000);
     renderPlanner()
 
-
-    // $("#9AM").addClass("past");
     // EVENT LISTENERS
-
-
+    $( ".saveBtn" ).click(function() {
+        for(i=0; i<9; i++){
+            var time = twelveHr(9 + i)
+            myStorage.setItem(time, $("#"+time).val());
+        }
+    });
 });
