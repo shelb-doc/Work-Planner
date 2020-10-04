cb82c88099bc626455f7dd27bc4cce186651c13f
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-03 22:13:00 -0400

    made table for time

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,4 +5,17 @@ $(document).ready(function () {
     // FUNCTION DEFINITIONS
     // FUNCTION CALLS
     // EVENT LISTENERS
+
+    // make date appear on jumbo
+    // creates the layout of the tables
+    var content = "<table>"
+    for(i=0; i<9; i++){
+        content += '<tr><td>' + (9 + i) + '</td>';
+        content += '<td>' + '<textarea id="meetingName'+ (9 + i) +'"></textarea>' + '</td>';
+        content += '<td>' + '<button type="button" class="btn btn-primary btn-lg">Save</button>' + '</td></tr>';
+    }
+    content += "</table>"
+    $('.container').append(content);
+        // make 3 col 
+        // times 
 });
