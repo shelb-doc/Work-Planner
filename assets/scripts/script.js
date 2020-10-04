d30591c88670b6de7aef728234ee61c0260ce3d1
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-04 18:41:03 -0400

    added a disable teaxt area function to disable the field when marked as past

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,21 +17,25 @@ $(document).ready(function () {
     function renderPlanner(){
         var currentTime = moment().format('H');
         var currentClass = "";
+        var isDisabled = "";
         for(i=0; i<9; i++){
             var time = twelveHr(9 + i)
             if (currentTime > (9 + i)){
                 currentClass = "past";
+                isDisabled = "disabled";
             } else if (currentTime == (9 + i)) {
                 currentClass = "present";
+                isDisabled = "";
             } else {
                 currentClass = "future";
+                isDisabled = "";
             }
             if(myStorage.getItem(time) == null){
                 myStorage.setItem(time, " ");
             }
             var content = '<form class="row">'
             content += '<div class="col-md-1 hour">' + (time) + '</div>';
-            content += '<div class="col-md-10 description p-0"><textarea class="'+ currentClass +'" id="'+ (time) +'">'+ myStorage.getItem(time) +'</textarea></div>';
+            content += '<div class="col-md-10 description p-0"><textarea '+ isDisabled +' class="'+ currentClass +'" id="'+ (time) +'">'+ myStorage.getItem(time) +'</textarea></div>';
             content += '<button class="col-md-1 saveBtn"><i class="far fa-save fa-lg"></i></button>';
             content += "</form>"
             $('.container').append(content);
