1ea4b55d58b9c18e2712bfe3ef731a1916daae77
Shelby Cignetti <shelby.cignetti@gmail.com>
2020-10-03 23:48:16 -0400

    made table into form the added clock to jumbotron

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,37 @@
 $(document).ready(function () {
     console.log("This is loading!");
+    console.log(moment().format('hA'))
     // DOM VARIABLES
     // JS VARIABLES
     // FUNCTION DEFINITIONS
+
+    function twelveHr(hour){
+        if (hour > 12){
+            return ((hour - 12) + "PM")
+        }
+        else {
+            return (hour + "AM")
+        }
+    }
+
+    function renderPlanner(){
+        for(i=0; i<9; i++){
+            var time = twelveHr(9 + i)
+            var content = '<form class="row">'
+            content += '<div class="col-md-1 hour">' + (time) + '</div>';
+            content += '<div class="col-md-10 description p-0"><textarea class="past" id="meetingName'+ (time) +'"></textarea>' + '</div>';
+            content += '<button class="col-md-1 saveBtn"><i class="far fa-save fa-lg"></i></button>';
+            content += "</form>"
+            $('.container').append(content);
+        }
+    }
+
     // FUNCTION CALLS
+
+    $("#currentDay").text (moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    renderPlanner()
     // EVENT LISTENERS
 
-    // make date appear on jumbo
-    // creates the layout of the tables
-    var content = "<table>"
-    for(i=0; i<9; i++){
-        content += '<tr><td>' + (9 + i) + '</td>';
-        content += '<td>' + '<textarea id="meetingName'+ (9 + i) +'"></textarea>' + '</td>';
-        content += '<td>' + '<button type="button" class="btn btn-primary btn-lg">Save</button>' + '</td></tr>';
-    }
-    content += "</table>"
-    $('.container').append(content);
-        // make 3 col 
-        // times 
+
 });
